f0b84614e3327b0fe9a313a583956bfe47fffacb
Nino Kristesiashvili <Nino Kristesiashvili>
2022-08-23 14:53:50 +0400

    added new message scroll

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { SocketService } from 'src/app/core/services/socket.service';
 
 @Component({
@@ -6,17 +14,35 @@ import { SocketService } from 'src/app/core/services/socket.service';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit, OnChanges {
   messages: string[] = [];
   constructor(private socketService: SocketService) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
   id: number;
+
+  messageBox: Element;
+
   ngOnInit(): void {
     this.socketService.listen('message').subscribe((data) => {
       console.log(data);
 
       this.id = data.id;
       this.messages.push(data.message);
+      this.scrollToBottom();
     });
     //subscribe to socket and get messages
+  }
+
+  ngAfterViewInit(): void {
+    this.messageBox = document.getElementsByClassName('messages')[0];
+    console.log(this.messageBox);
+  }
+
+  scrollToBottom() {
+    setTimeout(() => {
+      this.messageBox.scrollTop = this.messageBox.scrollHeight;
+    }, 0);
   }
 }
